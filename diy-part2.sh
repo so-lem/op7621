f1004359ed4306a46a0af9c8e0c688b537c36295
so-lem <101241888+so-lem@users.noreply.github.com>
2026-07-07 21:16:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,28 +3,25 @@
 # 设置默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# 默认主机名
+# 设置主机名
 sed -i 's/OpenWrt/CR6608-Docker/g' package/base-files/files/bin/config_generate
 
-# 添加软件包
+# Docker相关
 cat >> .config <<EOF
 
-CONFIG_PACKAGE_dockerd=y
 CONFIG_PACKAGE_docker=y
 CONFIG_PACKAGE_docker-compose=y
 CONFIG_PACKAGE_containerd=y
 CONFIG_PACKAGE_runc=y
 
-CONFIG_PACKAGE_luci-app-dockerman=y
-
+# 终端
 CONFIG_PACKAGE_ttyd=y
 
+# Samba
 CONFIG_PACKAGE_samba4-server=y
 
+# USB存储
 CONFIG_PACKAGE_kmod-usb-storage=y
 CONFIG_PACKAGE_kmod-usb-storage-uas=y
-
-CONFIG_PACKAGE_ntfs3-mount=y
-CONFIG_PACKAGE_exfatprogs=y
 
 EOF
